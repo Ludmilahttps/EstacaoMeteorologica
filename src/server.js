ff383dc0fb49ba8200391456c5951e4f7af7bdf2
Daleffe <105308194+Dleffe@users.noreply.github.com>
2024-05-17 17:02:50 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,23 @@
-import express, { json } from "express"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import router from "./routes/index.js";
+
+dotenv.config();
+
+const server = express();
+const PORT = process.env.PORT || 5008;
+
+server.use(cors({
+  origin: 'https://estacao-meteorologica-frontend.vercel.app',
+  optionsSuccessStatus: 200
+}));
+server.use(express.json());
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log(`Está rodando na porta ${PORT}`);
+});import express, { json } from "express"
 import dotenv from "dotenv"
 import cors from "cors"
 import router from "./routes/index.js"
