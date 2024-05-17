3690bec1bc5b99cdeb634b6c465d0d85e35596a0
Daleffe <105308194+Dleffe@users.noreply.github.com>
2024-05-17 15:44:52 -0300

    Update index.js

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -5,7 +5,7 @@ dotenv.config()
 const { Pool } = pg
 
 const databaseConfig = {
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.POSTGRES_URL,
   ssl: {
     rejectUnauthorized: false,
   },
