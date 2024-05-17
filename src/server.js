b870299c4b414efc4c77644073b38498f702a138
Daleffe <105308194+Dleffe@users.noreply.github.com>
2024-05-17 13:52:24 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,23 @@
-import express, { json } from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import router from "./routes/index.js"
+import express, { json } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import router from "./routes/index.js";
+import { Pool } from "pg";
 
-dotenv.config()
+dotenv.config();
 
-const server = express()
-const { PORT } = process.env || 5008
+const server = express();
+const { PORT, POSTGRES_HOST, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DATABASE } = process.env;
+const pool = new Pool({
+  user: POSTGRES_USER,
+  host: POSTGRES_HOST,
+  database: POSTGRES_DATABASE,
+  password: POSTGRES_PASSWORD,
+  port: 5432, // Porta padrão do PostgreSQL
+});
 
-server.use(cors())
-server.use(json())
-server.use(router)
+server.use(cors());
+server.use(json());
+server.use(router);
 
-server.listen(PORT, () => console.log(`Acesse em http://localhost:${PORT}`))
+server.listen(PORT, () => console.log(`Acesse em http://localhost:${PORT}`));
