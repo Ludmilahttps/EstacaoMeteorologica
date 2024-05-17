2a5fd8781b7413e46173e06a6f7b990d2dda6d4e
Daleffe <105308194+Dleffe@users.noreply.github.com>
2024-05-17 17:15:34 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { json } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import router from "./routes/index.js";
@@ -9,26 +9,13 @@ const server = express();
 const PORT = process.env.PORT || 5008;
 
 server.use(cors({
-  origin: 'https://estacao-meteorologica-frontend.vercel.app',
+  origin: 'https://estacao-meteorologica-frontend.vercel.app/',
   optionsSuccessStatus: 200
 }));
-server.use(express.json());
+server.use(json());
 server.use(router);
 
 server.listen(PORT, () => {
-  console.log(`Está rodando na porta ${PORT}`);
-});import express, { json } from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import router from "./routes/index.js"
+  console.log(`Server running on port ${PORT}`);
+});
 
-dotenv.config()
-
-const server = express()
-const { PORT } = process.env || 5008
-
-server.use(cors())
-server.use(json())
-server.use(router)
-
-server.listen(PORT, () => console.log(`Acesse em http://localhost:${PORT}`))
