0850921b59c3e6649712d4d0ce1d5927df3651b4
Daleffe <augustodaleffe@gmail.com>
2024-06-14 17:53:40 -0300

    adicionando temperatura ao querie

--- src/queries/bmpQuerie.js
+++ src/queries/bmpQuerie.js
@@ -2,9 +2,9 @@ export const insertData = () => {
         
         const query = `--sql
             INSERT INTO
-                public."BMP280" ("date", "pressure", "altitude", "idstation")
+                public."BMP280" ("date", "pressure","temperature" , "altitude", "idstation")
             VALUES
-                ($1, $2, $3, $4);        
+                ($1, $2, $3, $4, $5);        
         `
         return query
     }
