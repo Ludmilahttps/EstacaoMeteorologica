c8229194d2792f51e5a6f6b9d3190fb21fb31f9d
Daleffe <augustodaleffe@gmail.com>
2024-06-14 17:32:12 -0300

    corrigindo nome tabela DHT

--- src/queries/dhtQuerie.js
+++ src/queries/dhtQuerie.js
@@ -2,7 +2,7 @@ export const insertData = () => {
     
     const query = `--sql
           INSERT INTO
-              public."DHT11" ("date", "temperature", "humidity", "idStation")
+              public."DHT11" ("date", "temperature", "humidity", "idstation")
           VALUES
               ($1, $2, $3, $4);        
       `
