520bb57bfb10d3c01fdb32e844d2b46159a9a83f
Daleffe <augustodaleffe@gmail.com>
2024-06-14 16:41:59 -0300

    update index schemas

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,23 +1,60 @@
-import pg from "pg"
-import dotenv from "dotenv"
+import pg from "pg";
+import dotenv from "dotenv";
 
-dotenv.config()
-const { Pool } = pg
+dotenv.config();
+const { Pool } = pg;
 
 const databaseConfig = {
   connectionString: process.env.POSTGRES_URL,
   ssl: {
     rejectUnauthorized: false,
   },
-}
+};
 
-const connection = new Pool(databaseConfig)
+console.log("Database Config:", databaseConfig);  // Verifica se a configuração está correta
 
-export { connection }
-export * as stationSchema from "./stationSchema.js"
-export * as authSchema from "./authSchema.js"
-export * as pluviometerSchema from "./pluviometerSchema.js"
-export * as anemometerSchema from "./anemometerSchema.js"
-export * as dhtSchema from "./dhtSchema.js"
-export * as bmpSchema from "./bmpSchema.js"
-export * as queries from "../queries/index.js"
+const connection = new Pool(databaseConfig);
+
+connection.on('connect', () => {
+  console.log('Connected to the database');
+});
+
+connection.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
+export { connection };
+export * as stationSchema from "./stationSchema.js";
+export * as authSchema from "./authSchema.js";
+export * as pluviometerSchema from "./pluviometerSchema.js";
+export * as anemometerSchema from "./anemometerSchema.js";
+export * as dhtSchema from "./dhtSchema.js";
+export * as bmpSchema from "./bmpSchema.js";
+export * as queries from "../queries/index.js";
+
+
+
+// import pg from "pg"
+// import dotenv from "dotenv"
+
+// dotenv.config()
+// const { Pool } = pg
+
+// const databaseConfig = {
+//   connectionString: process.env.POSTGRES_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// }
+
+// const connection = new Pool(databaseConfig)
+
+// export { connection }
+// export * as stationSchema from "./stationSchema.js"
+// export * as authSchema from "./authSchema.js"
+// export * as pluviometerSchema from "./pluviometerSchema.js"
+// export * as anemometerSchema from "./anemometerSchema.js"
+// export * as dhtSchema from "./dhtSchema.js"
+// export * as bmpSchema from "./bmpSchema.js"
+// export * as queries from "../queries/index.js"
