e1668900b386b649071c481d20cb3714804cf231
Daleffe <augustodaleffe@gmail.com>
2024-06-14 10:15:05 -0300

    alterando schema para novo banco

--- src/schemas/stationSchema.js
+++ src/schemas/stationSchema.js
@@ -2,67 +2,66 @@ import Joi from "joi";
 import { connection } from "./index.js";
 import { querieUnidade } from "../queries/index.js";
 
-export const unidadeSchema = Joi.object({
-    localidade: Joi.string().required(),
-    estado: Joi.number().required(),
-    data_manutencao: Joi.date().optional(),
+export const stationSchema = Joi.object({
+    location: Joi.string().max(100).required(),
+    status: Joi.number().integer().min(0).max(1).required(),
+    lastCheckUp: Joi.date().required()
 });
 
-export const insertUnidade = async (unidade) => {
-    const { localidade, estado, data_manutencao } = unidade;
-    try {
-        await connection.query(querieUnidade.insertUnidade(), [localidade, estado, data_manutencao]);
-    } catch (error) {
-        console.log(error);
-        throw new Error("Error inserting unidade");
-    }
-};
-
-export const updateUnidade = async (id_unidade, updatedUnidade) => {
-    const { localidade, estado, data_manutencao } = updatedUnidade;
-    try {
-        await connection.query(querieUnidade.updateUnidade(), [localidade, estado, data_manutencao, id_unidade]);
-    } catch (error) {
-        console.log(error);
-        throw new Error("Error updating unidade");
-    }
-};
-
-export const deleteUnidade = async (id_unidade) => {
-    try {
-        await connection.query(querieUnidade.deleteUnidade(), [id_unidade]);
-    } catch (error) {
-        console.log(error);
-        throw new Error("Error deleting unidade");
-    }
-};
-
-export const getUnidadeById = async (id_unidade) => {
-    try {
-        const { rows } = await connection.query(querieUnidade.getUnidadeById(), [id_unidade]);
-        return rows[0];
-    } catch (error) {
-        console.log(error);
-        throw new Error("Error fetching unidade");
-    }
-};
-
-export const getUnidadeByManutencao = async (data_manutencao) => {
-    try {
-        const { rows } = await connection.query(querieUnidade.getUnidadeByManutencao(), [data_manutencao]);
-        return rows[0];
-    } catch (error) {
-        console.log(error);
-        throw new Error("Error fetching unidade");
-    }
-};
-
-export const getUnidadeByEstado = async (estado) => {
-    try {
-        const { rows } = await connection.query(querieUnidade.getUnidadeByEstado(), [estado]);
-        return rows[0];
-    } catch (error) {
-        console.log(error);
-        throw new Error("Error fetching unidade");
-    }
-};
+export const insertStation = () => {
+    const query = `--sql
+        INSERT INTO public.Station ("location", "status", "lastCheckUp")
+        VALUES ($1, $2, $3);
+    `;
+    return query;
+  };
+  
+  export const getStationById = () => {
+    const query = `--sql
+        SELECT * FROM public.Station
+        WHERE idStation = $1;
+    `;
+    return query;
+  };
+  
+  export const getStationByCheckUp = () => {
+    const query = `--sql
+        SELECT * FROM public."Station"
+        WHERE lastCheckUp = $1;
+    `;
+    return query;
+  };
+  
+  export const getStationByStatus = () => {
+    const query = `--sql
+        SELECT * FROM public."Station"
+        WHERE status = $1;
+    `;
+    return query;
+  };
+  
+  export const updateStationCheckup = () => {
+    const query = `--sql
+        UPDATE public."Station"
+        SET lastCheckUp = $1
+        WHERE idStation = $2;
+    `;
+    return query;
+  };
+  
+  export const updateStationStatus = () => {
+    const query = `--sql
+        UPDATE public."Station"
+        SET status = $1
+        WHERE idStation = $2;
+    `;
+    return query;
+  };
+  
+  export const deleteStation = () => {
+    const query = `--sql
+        DELETE FROM public."Station"
+        WHERE idStation = $1;
+    `;
+    return query;
+  };
