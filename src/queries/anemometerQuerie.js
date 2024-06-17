dfca38cead12a2c43661488fea3834cbeccfef2e
Daleffe <augustodaleffe@gmail.com>
2024-06-17 13:22:03 -0300

    ajustando nomes do json

--- src/queries/anemometerQuerie.js
+++ src/queries/anemometerQuerie.js
@@ -2,7 +2,7 @@ export const insertData = () => {
         
         const query = `--sql
             INSERT INTO
-                public."Anemometer" ("date", "wind_speed", "wind_direction", "wind_angle", "idstation")
+                public."Anemometer" ("date", "windspeed", "winddirection", "windangle", "idstation")
             VALUES
                 ($1, $2, $3, $4, $5);        
         `
