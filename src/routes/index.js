1dff6cbae6ed14ec8828895d682ba558a4cb70c8
Daleffe <augustodaleffe@gmail.com>
2024-06-21 12:08:40 -0300

    adicionando queries,schemas,endpoint para pegar as estacoes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import { pluviometerRoute } from "./pluviometerRoute.js"
 import { anemometerRoute } from "./anemometerRoute.js"
 import { dhtRoute } from "./dhtRoute.js"
 import { bmpRoute } from "./bmpRoute.js"
+import { stationRoute } from "./stationRoute.js"
 
 const router = Router()
 router.use([authRoute, dhtRoute, pluviometerRoute, anemometerRoute, bmpRoute, stationRoute])
